312ebc451d0754f2a4dc15c8d30f151877c003b9
lejulien-42 <re4chan@gmail.com>
2020-07-06 04:23:46 -0700

    ajout de commentaires

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,24 +6,47 @@
 /*   By: lejulien <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/05 21:08:15 by lejulien          #+#    #+#             */
-/*   Updated: 2020/07/05 22:22:28 by lejulien         ###   ########.fr       */
+/*   Updated: 2020/07/06 04:18:58 by lejulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _MINISHELL_H
 # define _MINISHELL_H
 
-// structure pour les variables globales du shell
+/*
+** structure pour les variables globales du shell
+*/
+
 typedef struct	s_shell
 {
 	char	prefix[64];
 	int		is_active;
 }				t_shell;
 
-// Liste chainée pour le parsing des entrées
+/*
+** Liste chainée pour le parsing des entrées
+*/
+
 typedef struct	s_entry
 {
 	char	c;
 	void	*next;
 }				t_entry;
+
+/*
+** Liste des variables d'environement
+**
+** contient le nom et la valeur d'une variable
+** et l'adresse de la suivante
+** visibility = 1 si var exported
+*/
+
+typedef struct	s_envars
+{
+	char	*var_name;
+	char	*var_value;
+	int		visibility;
+	void	*next;
+}				t_envars;
+
 #endif
