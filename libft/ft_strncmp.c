a0e32c3e2d12abb4b93c153c5e033c30b334d916
lejulien-42 <lejulien@student.42.fr>
2020-07-25 16:32:47 +0200

    modify ft_strncmp

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -26,8 +26,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 		else
 			return ((unsigned char)s1[index] - (unsigned char)s2[index]);
 	}
-	if (index == (int)n)
-		return (0);
 	if (s1[index] == '\0' || s2[index] == '\0')
 	{
 		return ((unsigned char)s1[index] - (unsigned char)s2[index]);
