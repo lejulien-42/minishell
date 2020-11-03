58ef74a912469d365ce82f2c3633f7c8ae6d2f65
lejulien-42 <lejulien@student.42.fr>
2020-11-03 16:21:17 +0100

    making clean fork.c

--- srcs/fork.c
+++ srcs/fork.c
@@ -6,7 +6,7 @@
 /*   By: lejulien <lejulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/29 16:46:22 by lejulien          #+#    #+#             */
-/*   Updated: 2020/11/03 16:05:26 by lejulien         ###   ########.fr       */
+/*   Updated: 2020/11/03 16:16:58 by lejulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,48 +23,42 @@ static int
 	int		is_pipe;
 
 	is_pipe = 0;
-	if (node->sep  && ft_strncmp(node->sep, "|", ft_strlen(node->sep)) == 0 ||
+	if ((node->sep  && ft_strncmp(node->sep, "|", ft_strlen(node->sep)) == 0) ||
 		node->is_next_pipe)
 	{
 		is_pipe = 1;
-		if (pipe(node->pipes))
-			return (0);
 		if (node->next && node->sep && ft_strncmp(node->sep, "|", ft_strlen(node->sep)) == 0)
 			node->next->is_next_pipe = 1;
+		if (pipe(node->pipes))
+			return (0);
 		node->is_next_pipe = 1;
 	}
 	pid = fork();
-	if (pid == 0)
+	if (pid < 0)
+		return (0);
+	else if (pid == 0)
 	{
-		if (node->is_next_pipe &&
-			dup2(node->pipes[1], 1) < 0)
+		if (node->is_next_pipe && dup2(node->pipes[1], 1) < 0)
 			return (0);
-		if (node->prev != NULL)
-		{
-			if (node->prev->is_next_pipe == 1)
-			{
-				if (dup2(node->prev->pipes[0], 0) < 0)
-					return (0);
-			}
-		}
+		if (node->prev && node->prev->is_next_pipe == 1 &&
+			dup2(node->prev->pipes[0], 0) < 0)
+			return (0);
 		if ((ret = execve(path, av, envp)) < 0)
 			return (0);
 		exit(ret);
 	}
-	else if (pid != -1)
+	else
 	{
 		waitpid(pid, &status, 0);
 		if (is_pipe)
 		{
 			close(node->pipes[1]);
-			if (!node->next)
+			if (!node->next || (node->sep &&
+				ft_strncmp(node->sep, ";", ft_strlen(node->sep)) == 0))
 				close(node->pipes[0]);
 		}
-		if (node->prev != NULL)
-		{
-			if (node->prev->is_next_pipe)
-				close(node->prev->pipes[0]);
-		}
+		if (node->prev && node->prev->is_next_pipe)
+			close(node->prev->pipes[0]);
 		if (WIFEXITED(status))
 			ret = WIFEXITED(status);
 	}
