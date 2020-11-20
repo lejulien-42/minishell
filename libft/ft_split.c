f9890140275e8e6f3849e28194a4063218d90414
François-Louis Talleux <francois@AirdeFroisLouis.home>
2020-11-20 16:50:08 +0100

    maybe  finish

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -1,58 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frtalleu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/22 06:15:17 by frtalleu          #+#    #+#             */
+/*   Updated: 2020/11/20 16:49:25 by frtalleu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "./libft.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-static int nb_word(char *str, char c)
+static int	nb_word(char *str, char c)
 {
-		int i;
-		int nb;
-		i = 0;
-		nb = 0;
-		while (str[i] != '\0')
-		{
-				if (str[i] != c)
-						nb++;
-				while (str[i] != c && str[i] != '\0')
-						i++;
-				while (str[i] == c)
-						i++;
-		}
-		return (nb);
+	int i;
+	int nb;
+
+	i = 0;
+	nb = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] != c)
+			nb++;
+		while (str[i] != c && str[i] != '\0')
+			i++;
+		while (str[i] == c)
+			i++;
+	}
+	return (nb);
 }
 
-char **ft_split2(char *str, char *c, int i)
+char		**ft_split2(char *str, char *c, int i)
 {
-		int j;
-		int word;
-		char **tab;
+	int		j;
+	int		word;
+	char	**tab;
+
+	j = 0;
+	word = nb_word(str, c[0]);
+	if (!(tab = malloc(sizeof(char *) * (word + 1))))
+		return (NULL);
+	while (!(word = 0) && str[i] == c[0])
+		i++;
+	while (str[i] != '\0')
+	{
+		while (str[i + j] != c[0] && str[i + j] != '\0')
+			j++;
+		while (str[i + j] == c[0])
+			j++;
+		if (str[i + j - 1] == c[0])
+			str[i + j - 1] = '\0';
+		tab[word++] = ft_strtrim(&str[i], c);
+		i = i + j;
 		j = 0;
-		word = nb_word(str, c[0]);
-		if (!(tab = malloc(sizeof(char *) * (word + 1))))
-				return (NULL);
-		while (!(word = 0) && str[i] == c[0])
-				i++;
-		while (str[i] != '\0')
-		{
-				while (str[i + j] != c[0] && str[i + j] != '\0')
-						j++;
-				while (str[i + j] == c[0])
-						j++;
-				if (str[i + j - 1] == c[0])
-						str[i + j - 1] = '\0';
-				tab[word++] = ft_strtrim(&str[i], c);
-				i = i + j;
-				j = 0;
-		}
-		tab[word] = 0;
-		return(tab);
+	}
+	tab[word] = 0;
+	return (tab);
 }
-char **ft_split(const char *str, char *c)
+
+char		**ft_split(const char *str, char *c)
 {
-		char *st;
-		char **tab;
-		st = ft_strdup(str);
-		tab = ft_split2(st, c, 0);
-		free(st);
-		return (tab);
+	char *st;
+	char **tab;
+
+	st = ft_strdup(str);
+	tab = ft_split2(st, c, 0);
+	free(st);
+	return (tab);
 }
